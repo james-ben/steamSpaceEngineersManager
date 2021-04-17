56a035e920653f87508258de3c683316b8b215d0
Benjamin James <bjames@byu.net>
2021-04-17 15:59:28 -0600

    Able to add text to images. Not very configurable yet.

--- scripts/images.py
+++ scripts/images.py
@@ -7,10 +7,12 @@ Each local workshop directory will have a sub-directory named "img".
 This will contain the screenshots to be used as thumbnail and other
  attached images on the workshop page.
 The raw image to be used as the thumbnail should be named 'thumb.png'.
+If thumb.png does not exist, then the JSON will indicate which image is
+ supposed to be used as the thumbnail, by tagging it with "thumb" attribute.
 This will be converted to the proper size, have a logo added, and overwrite
  existing 'thumb.png' file in the top BP directory.
-Any images that should have a logo (and optionally text) added to them
- should end in '-append-logo'.
+Any images that should have a logo and/or text added to them will have the
+ necessary options defined in the JSON.
 """
 
 # Notes:
@@ -49,6 +51,29 @@ class ImageSet:
             shlex.quote('{dst}'.format(dst=dst_path))
         ]
 
+    @staticmethod
+    def make_text_cmd(text, img_path, img_size, dst_path):
+        return [
+            'powershell.exe',
+            _magick_path,
+            'convert',
+            '-background',
+            shlex.quote('#00000080'),       # alpha channel, mostly transparent
+            '-fill',
+            'white',
+            '-size',
+            '700x60',
+            '-pointsize',
+            '48',
+            '-geometry',
+            '+900+50',
+            'caption:"{txt}"'.format(txt=text),
+            shlex.quote('{img}[{size}]'.format(img=img_path, size=img_size)),
+            '+swap',
+            '-composite',
+            shlex.quote(str(dst_path))
+        ]
+
     def get_thumb_info(self, cfg):
         if 'thumb' in cfg:
             thumb_cfg = cfg['thumb']
@@ -81,12 +106,39 @@ class ImageSet:
                                       self.dst_size, img_dst)
         proc = sp.Popen(cmd)
         proc.wait()
+        return proc.returncode
+
+    def add_text(self, img_path, text_cfg):
+        # self.make_text_cmd(grid_name, ...)
+        img_dst = img_path.parent / '{}_new.png'.format(img_path.stem)
+        # might have already been created, keep adding to new image
+        if img_dst.is_file():
+            img_path = img_dst
+        cmd = self.make_text_cmd(text_cfg['content'], img_path, self.dst_size, img_dst)
+
+        proc = sp.Popen(cmd)
+        proc.wait()
+        return proc.returncode
 
     def generate_thumbnail(self):
         thumb_src = self.img_path / 'thumb.png'
-        if not thumb_src.is_file():
-            return None
-        thumb_type, thumb_pos = self.get_thumb_info(self.cfg)
+        if thumb_src.is_file():
+            # found one, get info
+            thumb_type, thumb_pos = self.get_thumb_info(self.cfg)
+        else:
+            # nothing named thumb, maybe reuse other image
+            thumb_src = None
+            thumb_pos = self.default_position
+            # find one that has the attribute
+            for k, v in self.cfg.items():
+                if 'thumb' in v:
+                    thumb_src = self.img_path / '{}.png'.format(k)
+                    thumb_pos = v['thumb']
+            if thumb_src is None:
+                # couldn't find one at all
+                return None
+            thumb_type = self.default_thumb_type
+
         thumb_dst = self.dir_path / 'thumb.png'
         mask_path = self.logo_path / '{type}-thumb-{pos}.png'.format(type=thumb_type, pos=thumb_pos)
 
@@ -94,8 +146,7 @@ class ImageSet:
                                       self.thumb_size, thumb_dst)
         proc = sp.Popen(cmd)
         proc.wait()
-
-        return None
+        return proc.returncode
 
     def format_images(self):
         # do any of the images match in the cfg dict?
@@ -107,11 +158,7 @@ class ImageSet:
                 # apply the formatting
                 if 'logo' in v:
                     self.add_logo(img_path, 'logo-complex', v['logo'])
-                # elif 'logo-simple' in v:
-                #     self.add_logo(img_path, 'logo-simple', v['logo-simple'])
-                # TODO: add simple masks for full size
-                elif 'text' in v:
-                    pass
-                # TODO: adding text is more difficult
+                if 'text' in v:
+                    self.add_text(img_path, v['text'])
 
         return None
